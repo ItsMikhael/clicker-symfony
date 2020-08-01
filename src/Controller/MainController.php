dccd9d74baf10bd4e1ed2b891703c871b3488223
ItsMikhael <mstefanski.entrepreneur@gmail.com>
2020-08-01 16:58:56 +0200

    Added login and register pages(no functionality yet)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
+	 
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('/main/index.html.twig');
     }
+	
+	/**
+     * @Route("/custom/{name?}", name="custom")
+     */
+	
+	public function custom(Request $request) {
+		
+		$name = $request->get('name');
+		
+		return new Response('<html><body> inny pejcz ' . $name . ' </body></html> ');
+		
+	}
+	
+	public function login() {
+			return $this->redner('/security/login.html.twig');
+	}
 }
